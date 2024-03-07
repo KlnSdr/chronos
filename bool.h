78f83a693a6e2a22a22fd34c55f2413c027af348
KlnSdr <monsieurk1209@gmail.com>
2024-03-07 20:31:21 +0100

    replace stdbool with custom enum

---
+++ bool.h
@@ -0,0 +1,9 @@
+#ifndef BOOL_H
+#define BOOL_H
+
+typedef enum bool {
+    true,
+    false
+} bool;
+
+#endif
--- main.c
+++ main.c
@@ -1,8 +1,8 @@
-#define LOW false
 #define F_CPU 1000000UL
+#define LOW false
 #define HIGH true
 
-#include <stdbool.h>
+#include "bool.h"
 #include <avr/io.h>
 #include <util/delay.h>
 #include <avr/interrupt.h>
